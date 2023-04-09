33130a58a10684cf9805bbfe141cfda41d590dc6
Eric <49462890+DAQ121@users.noreply.github.com>
2023-04-09 14:10:25 +0800

    减少编写自定义函数时的冗余 (#3)
    * 减少自定义函数的冗余，使用Lambda表达式和一个通用的函数接口。在一个单独的类中实现多个自定义函数，然后通过Lambda表达式将它们注册到IParseFunction。
    
    * 减少自定义函数的冗余，使用Lambda表达式和一个通用的函数接口。在一个单独的类中实现多个自定义函数，然后通过Lambda表达式将它们注册到IParseFunction。

---
+++ oplog-example/src/main/java/com/elltor/example/config/CustomFunctionConfiguration.java
@@ -0,0 +1,37 @@
+package com.elltor.example.config;
+
+import com.elltor.example.config.Functions.CustomFunctions;
+import com.elltor.oplog.service.IParseFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+@Configuration
+public class CustomFunctionConfiguration {
+
+    private IParseFunction createParseFunction(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        try {
+            Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
+            return IParseFunction.fromLambda(method);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Bean
+    public IParseFunction judgingAdminFunction() {
+        return createParseFunction(CustomFunctions.class, "isAdmin", Integer.class);
+    }
+
+    @Bean
+    public IParseFunction userDetailParseFunction() {
+        return createParseFunction(CustomFunctions.class, "userDetail", String.class);
+    }
+
+    @Bean
+    public IParseFunction getOldOrderParseFunction() {
+        return createParseFunction(CustomFunctions.class, "getOldOrder", Long.class);
+    }
+}
